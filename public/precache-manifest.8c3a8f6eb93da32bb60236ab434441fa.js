5055fa019f7ea5c99e226963d45d1d5ffd474daf
Amelia Schulz <amelia.schulz@gmail.com>
2019-01-14 14:40:31 -0500

    adds public folder

---
+++ public/precache-manifest.8c3a8f6eb93da32bb60236ab434441fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3abdce0615193d097a3",
+    "url": "/static/css/main.229ec905.chunk.css"
+  },
+  {
+    "revision": "a3abdce0615193d097a3",
+    "url": "/static/js/main.a3abdce0.chunk.js"
+  },
+  {
+    "revision": "03cf1071a37a54245041",
+    "url": "/static/js/1.03cf1071.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "860b8a31fe72238c2956e921e989de03",
+    "url": "/static/media/background-black-close-up-1300578-min.860b8a31.jpg"
+  },
+  {
+    "revision": "5a1bf7d5056b42846dc179fcaec8c69b",
+    "url": "/index.html"
+  }
+];
